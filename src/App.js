d984a009924515f06c0b086d822e21b7cba7ab71
unknown <adetayoayodeji7@gmail.com>
2022-06-30 09:17:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+import {data}from './data'
+
+const url = 'https://course-api.com/react-tabs-project'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(1)
+
+  // const fetchJobs = async () => {
+  //   const response = await fetch(url);
+  //   const newJobs = await response.json();
+  //   setJobs(newJobs)
+  //   setLoading(false)
+  // }
+
+  const liar = () => {
+    setJobs(data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    liar()
+  }, [])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+  const { id, dates, title, duties,company} = jobs[value]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((item,index) => { 
+            return(
+              <button 
+              className= {`job-btn ${index === value && 'active-btn'}` }
+              onClick={() => setValue(index)}
+              key = {item.id}>
+                {item.company}
+              </button>
+            );
+          })}
+        </div>      
+        <article className="job-info" key={id}>
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div className="job-desc" key = {index}>
+              <FaAngleDoubleRight className ='job-icon'/>
+              <p>{duty}</p>
+            </div>
+          );
+        })}
+      </article>
+      </div>
+
+    </section>
   );
 }
 
 export default App;
+ 
